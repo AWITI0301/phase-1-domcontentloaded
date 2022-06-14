fbcb8263dd0b171a1b388462e3b4ffaa72456eda
AWITI0301 <johnoketchawiti@gmail.com>
2022-06-14 09:19:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    updateDOM()
+})
+function updateDOM(){
+    const edit = document.getElementById('text')
+    edit.innerHTML = "This is really cool!"
+    
+ 
+}
